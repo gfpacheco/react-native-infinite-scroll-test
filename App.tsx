ea3891aa2701c432294049e410dfe9058b8658bd
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2021-12-18 00:19:40 -0300

    Test infinite scroll

--- App.tsx
+++ App.tsx
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import faker from 'faker';
+import { useCallback, useState } from 'react';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+
+type DataItem = {
+  key: string;
+  title: string;
+  description: string;
+  image: string;
+};
+
+function generateTenItems(): DataItem[] {
+  return new Array(10).fill(undefined).map(() => ({
+    key: faker.datatype.uuid(),
+    title: faker.lorem.sentence(),
+    description: faker.lorem.sentences(),
+    image: `${faker.image.imageUrl()}?random=${Math.random()}`,
+  }));
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <List />
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+function List() {
+  const insets = useSafeAreaInsets();
+  const [data, setData] = useState(generateTenItems());
+
+  const loadMore = useCallback(() => {
+    setTimeout(() => {
+      setData([...data, ...generateTenItems()]);
+    }, 500);
+  }, [data]);
+
+  return (
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
+      data={data}
+      renderItem={({ item }) => <Item item={item} />}
+      onEndReached={loadMore}
+    />
+  );
+}
+
+function Item({ item }: { item: DataItem }) {
+  return (
+    <View style={styles.item}>
+      <Image style={styles.itemImage} source={{ uri: item.image }} />
+      <View style={styles.itemTexts}>
+        <Text style={styles.itemTitle}>{item.title}</Text>
+        <Text style={styles.itemDescription}>{item.description}</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +66,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  list: {
+    flex: 1,
+  },
+  item: {
+    flexDirection: 'row',
+    padding: 16,
+  },
+  itemImage: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover',
+  },
+  itemTexts: {
+    flex: 1,
+    marginLeft: 16,
+  },
+  itemTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  itemDescription: {},
 });
